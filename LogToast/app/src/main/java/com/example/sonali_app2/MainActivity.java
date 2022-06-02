d84d3663f0dad12aa76c56def6371c9d84ed1567
y-aditya2 <201b024@juetguna.in>
2022-06-02 18:28:22 +0530

    Lab Exercises.

---
+++ LogToast/app/src/main/java/com/example/sonali_app2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sonali_app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void display(View view){
+        Log.i("display","there is an error");
+        Toast.makeText(this,"hello",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
